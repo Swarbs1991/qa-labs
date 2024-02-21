e094c7b8a6a74b977fbf669691a3e9f9549d249c
Joe Swarbrick <git@swarbs.co.uk>
2024-02-21 14:36:04 +0000

    Changing JQuery for add to OL and Show/Hide

--- Module 1.4/JQuery Stuff/jq1.js
+++ Module 1.4/JQuery Stuff/jq1.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {
+$(function () {
     function addName() {
         // Use jQuery to extract the name the user has entered
         var userName = $('#userName').val();
 
         // Use jQuery to append the new name to the "names" div
-        $('#names').append('<div>' + userName + '</div>');
+        $('#names ol').append('<li>' + userName + '</li>')
 
         // Return false to prevent the form from submitting
         return false;
@@ -12,4 +12,8 @@ $(document).ready(function () {
 
     // Invoking addName function when the form is submitted
     $('form').submit(addName);
+
+    $(".btn").click(function() {
+            $('#names').toggleClass('hide');
+    });
 });
